2a48406b22b58688de282ccc2fce8d1e72219b77
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-05-06 15:44:33 +0200

    import bootstrap js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
 export function Header() {
 
     return (
      <>
-     <h1>Hi</h1>
+   <header>
+        <Navbar className="bg-white shadow-sm mb-3">
+          <Container>
+            <h1>Navbar</h1>
+
+          </Container>
+        </Navbar>
+      </header>
      </>
    );
  }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./scss/style.scss"
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
