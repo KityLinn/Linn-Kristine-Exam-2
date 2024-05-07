0c576cfe999e3dee3ed02d8feec36b5ea7109088
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-05-07 23:42:05 +0200

    working on header and login modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,46 @@
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container, Button, Offcanvas } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 
+
+
+
 export function Header() {
+  const [show, setShow] = useState(false);
 
-    return (
-     <>
-   <header>
-        <Navbar className="bg-white shadow-sm mb-3">
-          <Container>
-            <h1>Navbar</h1>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-          </Container>
-        </Navbar>
+  return (
+    <>
+      <header>
+        <Container>
+          <Navbar className="bg-white shadow-sm mb-3 d-md-none">
+            <Button
+              variant="outline-primary"
+              className=" border-2"
+              onClick={handleShow}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose} placement="end">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                Some text as placeholder. In real life you can have the elements
+                you have chosen. Like, text, images, lists, etc.
+              </Offcanvas.Body>
+            </Offcanvas>
+          </Navbar>
+          <Navbar className="d-none d-md-flex">
+            <h1>hi</h1>
+          </Navbar>
+        </Container>
       </header>
-     </>
-   );
- }
+    </>
+  );
+}
  
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,35 @@
+import { Navbar, Nav, Container, Button, Offcanvas, Modal } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
 export function Login() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
     return (
      <>
-     <h1>Login</h1>
+     <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     
      </>
    );
  }
