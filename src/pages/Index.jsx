2ee7bcb30cf733b88f807dd4ba3a2a69129e0fc5
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-05-08 19:56:16 +0200

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -10,71 +10,79 @@ export function Index() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function onSubmit(data) {
+  function loginSubmit(data) {
     console.log(data);
   }
 
-    return (
-      <>
-        <Row className="d-flex align-items-center justify-content-center">
-          <Form
-            onSubmit={handleSubmit(onSubmit)}
-            style={{ maxWidth: "600px" }}
-            className="border border-1 border-black p-3 rounded-1"
-          >
-            <h1>Login</h1>
-            <Form.Group className="mb-3" controlId="formName">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                className={errors.username && "error"}
-                type="text"
-                placeholder="Username"
-                {...register("username", {
-                  required: true,
-                  minLength: {
-                    value: 3,
-                    message: "Username must be more than 3 characters",
-                  },
-                })}
-              />
-              <p>{errors.username?.message}</p>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                className={errors.password && "error"}
-                type="text"
-                placeholder="Password"
-                {...register("password", {
-                  required: true,
-                  minLength: {
-                    value: 3,
-                    message: "Password must be more than 3 characters",
-                  },
-                })}
-              />
-              <p>{errors.password?.message}</p>
-            </Form.Group>
-            <div className="d-flex justify-content-between">
-              <Button variant="primary" type="submit">
-                Login
-              </Button>
-              <Button variant="primary" onClick={handleShow}>
-                Register
-              </Button>
-            </div>
-          </Form>
-        </Row>
+  function registerSubmit(data) {
+    console.log(data);
+  }
 
-        <Modal show={show} onHide={handleClose} animation={false} centered>
+  return (
+    <>
+      <Row className="d-flex align-items-center justify-content-center">
+        <Form
+          onSubmit={handleSubmit(loginSubmit)}
+          style={{ maxWidth: "600px" }}
+          className="border border-1 border-black p-3 rounded-1"
+        >
+          <h1>Login</h1>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              className={errors.username && "error"}
+              type="text"
+              placeholder="Username"
+              {...register("username", {
+                required: true,
+                minLength: {
+                  value: 3,
+                  message: "Username must be more than 3 characters",
+                },
+              })}
+            />
+            <p>{errors.username?.message}</p>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              className={errors.password && "error"}
+              type="text"
+              placeholder="Password"
+              {...register("password", {
+                required: true,
+                minLength: {
+                  value: 3,
+                  message: "Password must be more than 3 characters",
+                },
+              })}
+            />
+            <p>{errors.password?.message}</p>
+          </Form.Group>
+          <div className="d-flex justify-content-between">
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <Button variant="primary" onClick={handleShow}>
+              Register
+            </Button>
+          </div>
+        </Form>
+      </Row>
+
+      <Modal show={show} onHide={handleClose} animation={false} centered>
         <Modal.Header closeButton>
           <Modal.Title>Register</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form
-            onSubmit={handleSubmit(onSubmit)}
+          <Form
+            onSubmit={handleSubmit(registerSubmit)}
             style={{ maxWidth: "600px" }}
             className="border border-1 border-black p-3 rounded-1"
           >
@@ -136,31 +144,8 @@ export function Index() {
             </div>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-  
-        </Modal.Footer>
+        <Modal.Footer></Modal.Footer>
       </Modal>
-      </>
-    );
- }
- 
- /*
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose} animation={false} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-*/
+    </>
+  );
+}
